d0e14ecf12ab7c6da997d90bf4da27a68fa2cbbc
MaNicolas <48954140+MaNicolas@users.noreply.github.com>
2021-05-10 17:45:12 +0200

    test with ChromeOptions

--- src/main/java/com/lego/base/BaseTest.java
+++ src/main/java/com/lego/base/BaseTest.java
@@ -1,6 +1,7 @@
 package com.lego.base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
@@ -18,7 +19,9 @@ public class BaseTest {
 
 		BrowserDriverFactory factory = new BrowserDriverFactory(browser);
 		driver = factory.createDriver();
-		driver.manage().window().maximize();
+		//driver.manage().window().maximize();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors");
 	}
 
 	@AfterMethod(alwaysRun = true)
