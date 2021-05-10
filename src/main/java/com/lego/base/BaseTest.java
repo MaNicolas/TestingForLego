5c5d9bb981124bf22d9378da0b7c906833327073
MaNicolas <48954140+MaNicolas@users.noreply.github.com>
2021-05-10 18:04:12 +0200

    Back to initial settings

--- src/main/java/com/lego/base/BaseTest.java
+++ src/main/java/com/lego/base/BaseTest.java
@@ -1,8 +1,6 @@
 package com.lego.base;
 
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
@@ -20,10 +18,7 @@ public class BaseTest {
 
 		BrowserDriverFactory factory = new BrowserDriverFactory(browser);
 		driver = factory.createDriver();
-		//driver.manage().window().maximize();
-		driver.manage().window().setSize(new Dimension(1920,1080));
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--headless");
+		driver.manage().window().maximize();
 	}
 
 	@AfterMethod(alwaysRun = true)
